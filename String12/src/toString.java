1718443aafb80899efe56b9c67815ba42544c907
ivavsys <itms@ivavsys.com>
2021-01-04 11:52:10 +0530

    commit by kk

---
+++ String12/src/toString.java
@@ -0,0 +1,12 @@
+import org.omg.Messaging.SyncScopeHelper;
+
+public class toString {
+	
+	int sid;
+	String name;
+	String city;
+	public toString() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
